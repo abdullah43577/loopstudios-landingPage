f6bf101230c93b2a4b6fb12bb95b5c1fee124be9
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2022-11-04 20:45:40 +0100

    made final changes to the code, modifying the readme file and as well deleting some uneeded files.

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 "use strict";
 
 const open = document.querySelector('img[src="./images/icon-hamburger.svg"]');
+const list = document.querySelector("#navhd > ul");
+const close = document.querySelector('img[src="./images/icon-close.svg"]');
 
 open.addEventListener("click", () => {
-  console.log("I'm the open button");
+  // console.log("I'm the open button");
+  list.classList.toggle("show");
+  open.classList.add("hidden");
+  open.classList.remove("hamburger");
+  close.classList.add("hamburger");
+  close.style.width = "40px";
+});
+
+close.addEventListener("click", () => {
+  //console.log("I'm being clicked");
+  list.classList.toggle("show");
+  close.classList.add("hidden");
+  close.classList.remove("hamburger");
+  open.classList.add("hamburger");
 });
